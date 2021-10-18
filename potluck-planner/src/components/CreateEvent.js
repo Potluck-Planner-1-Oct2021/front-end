2b36fabf649accde78b6e81e7b13121cf0b96c55
David - Lambda School User <david-lambdaschooluser@davids-mbp.lan>
2021-10-18 15:13:22 -0400

    form validation added

--- potluck-planner/src/components/CreateEvent.js
+++ potluck-planner/src/components/CreateEvent.js
@@ -1,6 +1,14 @@
 import React, {useEffect, useState} from "react";
 import * as yup from 'yup'
 
+  const schema =yup.object().shape({
+    name: yup.string().required('Please provide a name for your event'),
+    date: yup.string().required('Please let your guets know when your potluck will be'),
+    location: yup.string().required('Please let your guests know where your potluck will be'),
+    // food: ,
+    invites: yup.string()
+  })
+
 function CreateEvent() {
 
   const initialFormValues = {name: '', date: '', location: '', food: [], invites: ''}
@@ -13,13 +21,25 @@ function CreateEvent() {
     const { name, type, value, checked} = event.target
     const valueToUse = type === 'checkbox' ? checked : value
     setForm({...form, [name]: valueToUse})
-    //setFormErrors(name,valueToUse)
-}
+    setFormErrors(name,valueToUse)
+  }
 
+  const setFormErrors = (name,value) => {
+    yup.reach(schema,name).validate(value)
+        .then(() => setErrors({...errors, [name]: ''}))
+        .catch( err => setErrors({...errors, [name]: err.errors[0]}))
+  }
+
+    useEffect(() => {
+      schema.isValid(form).then(valid => setDisabled(!valid))
+  }, [form])
 
   return (
     <div className="App">
       <form>
+        <div>{errors.name}</div>
+        <div>{errors.date}</div>
+        <div>{errors.location}</div>
         <label> Event Name
             <input onChange={change} value={form.name} name="name" type="text"/>
         </label>
@@ -39,6 +59,7 @@ function CreateEvent() {
         <label> Invite Friends (comma-separated list of emails)
           <input onChange={change} value={form.invites} name="invites" type="text"/>
         </label>
+        <button disabled={disabled}>Submit</button>
       </form>
     </div>
   );
