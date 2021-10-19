acf44b74eaf6ba2318436e9275248704d0b312a8
Kat <kyevsukov@gmail.com>
2021-10-19 13:52:19 -0400

    test login component built

---
+++ potluck-planner/src/components/Login.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+
+
+export default function Login() {
+  
+  const [formValues, setFormValues] = useState({ username: "", password: "" });
+
+
+  const onChange = (e) => {
+      setFormValues({...formValues, [e.target.name]: e.target.value})
+  }
+
+
+  const handleLogin = (e) => {
+      e.preventDefault()
+      axios.post('https://potluckaapi.herokuapp.com/api/auth/login', formValues)
+      .then(res => {
+          console.log(res)
+      })
+      .catch(err => {
+          console.log(err.message)
+      })
+  }
+  return (
+    <form onSubmit={handleLogin}>
+      <label htmlFor="username">
+        <input id='username' name="username" type="text" placeholder="username" value={formValues.username} onChange={onChange}/>
+        <label htmlFor="password">
+          <input id='password' name="password" type="password" placeholder="password" value={formValues.password}  onChange={onChange}/>
+        </label>
+        <button>Login</button>
+      </label>
+    </form>
+  );
+}
