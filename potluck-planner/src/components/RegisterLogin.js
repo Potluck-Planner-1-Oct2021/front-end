87d24c11432fbeaf429f120fd829d790aebe959e
astephens-fsd <astephens.pm@icloud.com>
2021-10-20 11:45:47 -0700

    Added styles to RegisterLogin.js

---
+++ potluck-planner/src/components/RegisterLogin.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import * as yup from 'yup'
+
+
+
+let styles = {
+    formLabel: {
+        text: 'bold',
+        fontSize: '25px',
+        color: #105773
+    },
+    errorLabel: {
+        text: 'bold',
+        fontSize: '16px',
+        color: 'red'
+    },
+    underlineStyle: {
+        borderColor: #F2B441
+    },
+    input {
+        fontSize: '19px'
+        marginTop: '20px'
+        backgroundColor: 'white',
+        color: '#61788C'
+    },
+    button {
+      marginBottom: '0px',
+      padding: '10px',
+      textAlign: 'center',
+      color: '#105773',
+      backgroundColor: '#F2B441'
+    }
+};
