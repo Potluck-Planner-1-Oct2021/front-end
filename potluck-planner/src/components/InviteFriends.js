872b6215ff43b682b2852bf8ae64d837b87a077f
Kat <kyevsukov@gmail.com>
2021-10-21 18:18:33 -0400

    finished incorporating davids styling

--- potluck-planner/src/components/InviteFriends.js
+++ potluck-planner/src/components/InviteFriends.js
@@ -1,25 +1,56 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
+const LabelInputDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  //border: 1px solid red;
+  width: 40%;
+  padding-left: 1%;
+`;
+
+const LabelDiv = styled.div`
+  //border: 1px solid green;
+  width: 50%;
+  text-align: left;
+`;
+
+// const InputDiv = styled.div`
+//   //border: 1px solid purple;//
+// `;
+
+// const StyledLabel = styled.label`
+//   //border: 2px solid blue;
+//   display: block;
+// `;
 //not required
 //just requests a text string
 //label directs user to provide a comma-separated list of emails
 
 function InviteFriends({ formToSubmit, setFormToSubmit }) {
-
-  const [friendsList, setFriendsList] = useState('')
+  const [friendsList, setFriendsList] = useState("");
 
   const change = (event) => {
-    setFriendsList(event.target.value)
-    const arrayList = friendsList.split(',')
-    setFormToSubmit({...formToSubmit, guests: arrayList})
-  }
+    setFriendsList(event.target.value);
+    const arrayList = friendsList.split(",");
+    setFormToSubmit({ ...formToSubmit, guests: arrayList });
+  };
 
   return (
     <div className="App">
       <form>
-        <label> Invite Friends (comma-separated list of emails)
-          <input onChange={change} value={friendsList} name="friendsList" type="text"/>
-        </label>
+        <LabelInputDiv>
+          <LabelDiv>
+            <label> Invite Friends</label>
+            <label> (comma-separated list of emails)</label>
+          </LabelDiv>
+          <input
+            onChange={change}
+            value={friendsList}
+            name="friendsList"
+            type="text"
+          />
+        </LabelInputDiv>
       </form>
     </div>
   );
--- potluck-planner/src/components/ViewEvent.js
+++ potluck-planner/src/components/ViewEvent.js
@@ -3,24 +3,35 @@ import EventBasics from "./EventBasics";
 import InviteFriends from "./InviteFriends";
 import RSVP from "./RSVP";
 import ViewFoods from "./ViewFoods";
+import styled from "styled-components";
 
+const EventContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background-color: #105773;
+  color: #f2b441;
+  height: 100vh;
+`;
 
+const EventDiv = styled.div`
+  width: 50%;
+  border: 1px solid green;
+`;
 
 function ViewEvent() {
-
-    const [isOrganizer, setIsOrganizer]=useState(false)
+  const [isOrganizer, setIsOrganizer] = useState(false);
 
   return (
-    <div className="App">
-      <EventBasics editable={isOrganizer}></EventBasics>
-      {
-          isOrganizer && <InviteFriends></InviteFriends>
-      }
-      {
-          !isOrganizer && <RSVP></RSVP>
-      }
-      <ViewFoods changeAll={isOrganizer}></ViewFoods>
-    </div>
+    <EventContainer>
+      <EventDiv>
+        <EventBasics editable={isOrganizer}></EventBasics>
+        {isOrganizer && <InviteFriends></InviteFriends>}
+        {!isOrganizer && <RSVP></RSVP>}
+        <ViewFoods changeAll={isOrganizer}></ViewFoods>
+      </EventDiv>
+    </EventContainer>
   );
 }
 
