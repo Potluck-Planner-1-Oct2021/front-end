be7c72d87cf6cdb23f35b19f9554caf811000c64
Kat <kyevsukov@gmail.com>
2021-10-21 15:51:38 -0400

    created fetch events action

--- potluck-planner/src/actions/eventActions.js
+++ potluck-planner/src/actions/eventActions.js
@@ -1,2 +1,20 @@
+import axiosWithAuth from "../utils/axiosWithAuth"
+
 export const FETCH_HOSTING_SUCCESS = "FETCH_HOSTING_SUCCESS"
 
+export const getHostedEvents = () => {
+    return (dispatch) => {
+        axiosWithAuth()
+    .get('/potlucks')
+    .then(res => {
+        dispatch(hostedSuccess(res.data))
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    }
+}
+
+export const hostedSuccess = (events) => {
+    return({type: FETCH_HOSTING_SUCCESS, payload: events})
+}
